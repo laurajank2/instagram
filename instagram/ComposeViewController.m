c9592e11bf7247bb053a1be82f295c9b9badffd0
laurajank2 <67391108+laurajank2@users.noreply.github.com>
2022-06-27 15:25:50 -0700

    resize image implemented

--- instagram/ComposeViewController.m
+++ instagram/ComposeViewController.m
@@ -34,11 +34,26 @@
     [self presentViewController:imagePickerVC animated:YES completion:nil];
 }
 
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
     
     // Get the image captured by the UIImagePickerController
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
     UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    [self resizeImage:originalImage withSize:self.toPostImage.bounds.size];
     self.toPostImage.image = originalImage;
 
     // Do something with the images (based on your use case)
@@ -46,6 +61,8 @@
     // Dismiss UIImagePickerController to go back to your original view controller
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+
 
 - (IBAction)postThis:(id)sender {
     [Post postUserImage:self.toPostImage.image withCaption:self.toPostCaption.text withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
