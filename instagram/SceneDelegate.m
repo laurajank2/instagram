d3355cb9c75e30f1e7d9f258510d14a8491b6e9d
laurajank2 <67391108+laurajank2@users.noreply.github.com>
2022-06-28 15:05:02 -0700

    tab navigation and better launch screen

--- instagram/SceneDelegate.m
+++ instagram/SceneDelegate.m
@@ -22,7 +22,7 @@
     if (PFUser.currentUser) {
            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     
-           self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"HomeNav"];
+           self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"TabControl"];
        }
 }
 
